8460274379a1f57666cfc7a3a9d11c4a903a7df6
Jay Kay <60857616+newjessar@users.noreply.github.com>
2022-10-30 23:08:34 +0100

    foo updated, required to add time

--- foo.py
+++ foo.py
@@ -1,15 +1,29 @@
 import mdp
-
-# n = input("Choose the Run 1 or 2: ")
-# if (n == "1"):
+while(True):
+    i = input("What type of map do you like to Rrn: RN-> inter 1 or 2D-> inter 2 or 3 for exit:   ")
+    if (i == "1"):
+        n = input("Choose the Run Value-> inter 1 or Policy-> inter 2:   ")
+        if (n == "1"):
+            m1 = mdp.makeRNProblem()
+            m1.valueIteration()
+            m1.printValues()
+        else:
+            if (n == "2"):
+                m2 = mdp.makeRNProblem()
+                m2.policyIteration()
+                m2.printActions()
     
-# m1 = mdp.makeRNProblem()
-# m1.valueIteration()
-# m1.printValues()
-# else:
-#     if (n == "2"):
-m2 = mdp.makeRNProblem()
-m2.policyIteration()
-m2.printValues()
-m2.printActions()
-        
+    elif (i == "2"):        
+            n = input("Choose the Run Value-> inter 1 or Policy-> inter 2:   ")
+            if (n == "1"):
+                m1 = mdp.make2DProblem()
+                m1.valueIteration()
+                m1.printValues()
+            else:
+                if (n == "2"):
+                    m2 = mdp.make2DProblem()
+                    m2.policyIteration()
+                    m2.printActions()
+    else:
+        if (i == "3"): 
+            break
