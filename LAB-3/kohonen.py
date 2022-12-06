f17ba2a4bd3b7fd7dac19ad857dec17e28a76764
Jay Kay <60857616+newjessar@users.noreply.github.com>
2022-12-06 02:06:59 +0100

    really Done

--- LAB-3/kohonen.py
+++ LAB-3/kohonen.py
@@ -1,3 +1,5 @@
+import math
+
 class Cluster:
     """This class represents the clusters, it contains the
     prototype and a set with the ID's (which are Integer objects) 
@@ -17,58 +19,54 @@ class Kohonen:
         # A 2-dimensional list of clusters. Size == N x N
         self.clusters = [[Cluster(dim) for _ in range(n)] for _ in range(n)]
         # Threshold above which the corresponding html is prefetched
-        self.prefetch_threshold = 0.5
+        self.prefetch_threshold = 0.7
         self.initial_learning_rate = 0.8
         # The accuracy and hitrate are the performance metrics (i.e. the results)
         self.accuracy = 0
         self.hitrate = 0
-
+    
+    ## Measures the Euclidean Distances
+    def euclidean_distance(self, p1, p2):
+        return(sum([(a-b)**2 for a, b in zip(p1, p2)]))
 
     def train(self):
-        ## Step 1: Each cluster center is randomly initialized.
+
+        ## Step 1: Iterate every epoch
         for inEpoch in range(self.epochs):      
-            
-            ## The radius r of the neighborhood of the BMU is calculated
+            ## The radius r of the BMU's neighborhood is calculated
             r = round((self.n/2)*(1-inEpoch/self.epochs),2)
+            ## Learning rate calculation according to the equation
+            eTa = (self.initial_learning_rate*(1-inEpoch/self.epochs))
             
-            ## Step 2: For all vectors from the set: The vector is presented to the map
+            
+            ## Step 2: Calculating the square size and the learning rate eTa
             for vInput in self.traindata:
                 eucl_dist = None
-                ## Step 3: All nodes in the map (which represent cluster centers) are examined to find the closest to the input vector 
-                # in terms of the Euclidean distance. The winning node is known as the Best Matching Unit (BMU).
+                
+                ## Step 3: Find the BMI related to the given vector
                 for clusterRow_IDX, clusterRow in enumerate(self.clusters):
                     for clusterValue_idx, clusterValue in enumerate(clusterRow):
-                        cluster_diff = 0    
-                        for dim_idx in range(self.dim):
-                            cluster_diff += pow(clusterValue.prototype[dim_idx] - vInput[dim_idx],2)
+                        cluster_diff = self.euclidean_distance(clusterValue.prototype, vInput)
                         if eucl_dist == None or eucl_dist > cluster_diff:
                             eucl_dist = cluster_diff
                             bmu_node_idx, bmu_in = clusterRow_IDX, clusterValue_idx 
                             
-                ## Step 4: set to half the “size” of the map, but diminishes with each training epoch. All the 
-                # nodes found within this radius are inside the BMU’s neighborhood.
+                ## Step 4: Each node in BMU’s neighborhood is adjusted
                 for clusterRow_IDX, clusterRow in enumerate(self.clusters):
                     if clusterRow_IDX < bmu_node_idx-r or clusterRow_IDX > bmu_node_idx+r:
-                        continue    
+                          continue    
                     
                     for nodeValue_idx, nodeValue in enumerate(clusterRow):
                         if nodeValue_idx < bmu_in-r or nodeValue_idx > bmu_in+r:
                             continue
                         
-                        ## Learning rate calculation according to the equation
-                        eTa = (0.8*(1-inEpoch/self.epochs))
-                        ## Step 5: Each node in BMU’s neighborhood (the nodes found in step 4) is 
-                        # adjusted to make it more like the input vector:
+                        ## Step 5: Update the weights pre_prototype following the equation in the assignment
                         for idx in range(self.dim):
-                            p_OLD = nodeValue.prototype[idx]
+                            pre_prototype = nodeValue.prototype[idx]
                             x = vInput[idx]
-                            p_NEW = (((1 - eTa)*p_OLD)+(eTa*x))
-                            nodeValue.prototype[idx] = p_NEW
+                            nodeValue.prototype[idx] = (((1 - eTa)*pre_prototype)+(eTa*x))
 
-                        
-        ## Step 6: Repeat steps 2, 3, 4, 5 running over the whole training dataset several 
-        # times (tmax) and at each training epoch decrease the radius of the neighborhood r 
-        # and the learning rate η.
+
 
     def test(self):
             
@@ -105,8 +103,6 @@ class Kohonen:
         if html_prefetched> 0: self.accuracy = (total_hits/ html_prefetched)
         
         self.print_test            
-            
-
 
     def print_test(self):
         print("Prefetch threshold =", self.prefetch_threshold)
